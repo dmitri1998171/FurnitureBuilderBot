925dc5f45225d4e5e3382b316b44ded5224ec694
dmitri1998171 <dmitri1998171@icloud.com>
2023-10-03 12:58:22 +0600

    code review

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import telebot, json
 
-moduleWidthes = [800, 600, 400]
-modules = {
-    '800':0,
-    '600':0,
-    '400':0
-}
+modules = { }
 other = 0
 
-def parseJSON():
-    tokens = {}
+def getSettingsFromJSON():
+    settings = {}
 
-    with open("token.json", 'r') as f:
-        tokens = json.load(f)
+    with open("settings.json", 'r') as f:
+        settings = json.load(f)
 
-    return tokens
+    return settings
 
-tokens = parseJSON()
-bot = telebot.TeleBot(tokens['tg'])
+settings = getSettingsFromJSON()
+moduleWidthes = settings['moduleWidthes']
+
+bot = telebot.TeleBot(settings['token'])
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -29,23 +26,30 @@ def get_text(message):
     if(message.text.isnumeric()):
         moduleCalc(int(message.text))
         showResults(message)
-
     else:
         bot.send_message(message.chat.id,"Это не число")
 
 def moduleCalc(width):
-    cycle = True
+    global other, modules
+
+    modules = {
+        '800':0,
+        '600':0,
+        '400':0
+    }
+    other = 0
     
+    cycle = True
     while cycle == True:
-        if(width > moduleWidthes[0]):
+        if(width >= moduleWidthes[0]):
             width = width - moduleWidthes[0]
             modules['800'] = modules['800'] + 1
 
-        elif(width > moduleWidthes[1]):
+        elif(width >= moduleWidthes[1]):
             width = width - moduleWidthes[1]
             modules['600'] = modules['600'] + 1
 
-        elif(width > moduleWidthes[2]):
+        elif(width >= moduleWidthes[2]):
             width = width - moduleWidthes[2]
             modules['400'] = modules['400'] + 1
 
@@ -57,11 +61,13 @@ def moduleCalc(width):
     print("other: ", other)
 
 def showResults(message):
+    global other
     resultMessage = 'Кол-во модулей\n\n'
 
     for item in modules:
         resultMessage += "С шириной " + item + " мм : " + str(modules[item]) + " шт.\n"
 
+    resultMessage += "\n" + str(other) + " мм осталось до края"
     bot.send_message(message.chat.id, resultMessage)
     
 
