237bf792ef3aba68a03f51df93c56b2f3c3d32bd
dmitri1998171 <dmitri1998171@icloud.com>
2023-10-03 11:25:55 +0600

    get width of a wall

--- main.py
+++ main.py
@@ -16,4 +16,12 @@ def start_message(message):
     bot.send_message(message.chat.id,"Привет ✌️ Я бот-сборщик мебели!")
     bot.send_message(message.chat.id,"Отправь мне ширину кухни - я все посчитаю")
 
+@bot.message_handler(content_types='text')
+def get_text(message):
+    if(message.text.isnumeric()):
+        print("It is number")
+
+def moduleCalc():
+    pass
+
 bot.infinity_polling()
